2fb58db4f6ebbc35c84317a7c53fd4bd0f59695f
coderxpb <borapankaj901@gmail.com>
2021-09-01 15:30:08 +0530

    Add operation functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -18,7 +18,7 @@ function Calculator(props){
 
 	const delValue = (e) =>{
 		e.preventDefault();
-		if(curValue !=''){
+		if(curValue !==''){
 			let tempVal = curValue;
 			tempVal = tempVal.substring(0,tempVal.length-1);
 			setCurValue(tempVal);
@@ -29,33 +29,73 @@ function Calculator(props){
 		e.preventDefault();
 		setCurValue('');
 		setPrevValue('');
+		setOperator('');
+	}
+
+	const calculateVal = () =>{
+		let floatPrev = parseFloat(prevValue);
+		let floatCur = parseFloat(curValue);
+		switch(operator){
+			case '+': return (floatPrev+floatCur);
+			case '-': return (floatPrev-floatCur);
+			case 'x': return (floatPrev*floatCur);
+			case '/': return (floatPrev/floatCur);
+			default: return null;
+		}
+	}
+
+	const operatorClicked = (e) =>{
+		e.preventDefault();
+		let curOperator = e.target.getAttribute('data-val');
+		if(curValue !== ''){
+			if(prevValue !== ''){
+				let newValue = String(calculateVal());
+				if (curOperator !=='='){
+					setPrevValue(newValue);
+					setOperator(curOperator);
+					setCurValue('');
+				}
+				else{
+					setPrevValue('');
+					setCurValue(newValue);
+					setOperator('');
+				}
+			}
+			else{
+				if(curOperator !== '='){
+					setPrevValue(curValue);
+					setOperator(e.target.getAttribute('data-val'));
+					setCurValue('');
+				}
+			}
+		}
 	}
 
 	return (
 		<div>
 			<CalcContainer>
 				<CalcScreen>
-					<PrevScreen>{prevValue}{operator}</PrevScreen>
+					<PrevScreen>{prevValue} {operator}</PrevScreen>
 					<CurScreen>{curValue}</CurScreen>
 				</CalcScreen>
 				<CalcButton onClick = {allClear} gridSize buttonColor="light">AC</CalcButton>
 				<CalcButton onClick = {delValue} buttonColor="light">DEL</CalcButton>
-				<CalcButton buttonColor="op">%</CalcButton>
+				<CalcButton data-val={'/'} onClick={operatorClicked} buttonColor="op">/</CalcButton>
 				<CalcButton data-val={'7'} onClick={appendValue}>7</CalcButton>
 				<CalcButton data-val={'8'} onClick={appendValue}>8</CalcButton>
 				<CalcButton data-val={'9'} onClick={appendValue}>9</CalcButton>
-				<CalcButton buttonColor="op">x</CalcButton>
+				<CalcButton data-val={'x'} onClick={operatorClicked} buttonColor="op">x</CalcButton>
 				<CalcButton data-val={'4'} onClick={appendValue}>4</CalcButton>
 				<CalcButton data-val={'5'} onClick={appendValue}>5</CalcButton>
 				<CalcButton data-val={'6'} onClick={appendValue}>6</CalcButton>
-				<CalcButton buttonColor="op">-</CalcButton>
+				<CalcButton data-val={'-'} onClick={operatorClicked} buttonColor="op">-</CalcButton>
 				<CalcButton data-val={'1'} onClick={appendValue}>1</CalcButton>
 				<CalcButton data-val={'2'} onClick={appendValue}>2</CalcButton>
 				<CalcButton data-val={'3'} onClick={appendValue}>3</CalcButton>
-				<CalcButton buttonColor="op">+</CalcButton>
+				<CalcButton data-val={'+'} onClick={operatorClicked} buttonColor="op">+</CalcButton>
 				<CalcButton data-val={'.'} onClick={appendValue} buttonColor="light">.</CalcButton>
 				<CalcButton data-val={'0'} onClick={appendValue}>0</CalcButton>
-				<CalcButton gridSize buttonColor="op">=</CalcButton>
+				<CalcButton data-val={'='} onClick={operatorClicked} gridSize buttonColor="op">=</CalcButton>
 			</CalcContainer>
 		</div>
 	)
